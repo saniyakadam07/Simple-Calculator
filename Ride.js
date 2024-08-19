eb140250cb5c4409ea8cd8cc8a132182c208e47e
Saniya Kadam <175844725+saniya7125@users.noreply.github.com>
2024-08-19 11:45:52 +0530

    Delete Ride.js

--- Ride.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const rideSchema = new mongoose.Schema({
-    description: { type: String, required: true },
-    contact: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Ride', rideSchema);
