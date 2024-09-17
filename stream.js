fac50e309d675a553aac8ad3bf5e7fd523aacb45
Saniya Kadam <175844725+saniya7125@users.noreply.github.com>
2024-09-17 20:55:41 +0530

    Delete stream.js

--- stream.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const StreamSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String },
-  careers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Career' }]
-});
-
-module.exports = mongoose.model('Stream', StreamSchema);
